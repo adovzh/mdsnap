28a3e8d90a86ceee9cb97f4579844ff3b183bf15
Alexander Dovzhikov <alexander.dovzhikov@gmail.com>
2019-05-28 19:57:30 +1000

    job status started/completed

--- R/mdsnap.R
+++ R/mdsnap.R
@@ -2,6 +2,31 @@ library(DBI)
 library(RPostgreSQL)
 library(quantmod)
 
+open_job <- function(conn) {
+    # create a new job
+    seq_job <- dbGetQuery(conn, "SELECT nextval('seq_job') AS job_id")
+    rs <- dbSendStatement(conn, "INSERT INTO t_job (job_id) VALUES ($1)",
+                          param = list(seq_job$job_id))
+    jobs_inserted <- dbGetRowsAffected(rs)
+    dbClearResult(rs)
+
+    stopifnot(jobs_inserted == 1)
+    seq_job$job_id
+}
+
+complete_job <- function(conn, job_id) {
+    # complete a job
+    rs <- dbSendStatement(conn, "UPDATE t_job
+                          SET job_status_id=(SELECT job_status_id
+                          FROM t_job_status WHERE job_status_name='COMPLETED'),
+                          modified_on=current_timestamp where job_id=$1",
+                          param = list(job_id))
+    jobs_updated <- dbGetRowsAffected(rs)
+    dbClearResult(rs)
+
+    stopifnot(jobs_updated == 1)
+}
+
 mdsnap <- function(host, port, dbname, user, password) {
     conn <- dbConnect(RPostgreSQL::PostgreSQL(), user = user, password = password,
                       host = host, port = port, dbname = dbname)
@@ -11,15 +36,9 @@ mdsnap <- function(host, port, dbname, user, password) {
     securities <- dbGetQuery(conn, secQuery)
 
     # create a new job
-    seq_job <- dbGetQuery(conn, "SELECT nextval('seq_job') AS job_id")
-    rs <- dbSendStatement(conn, "INSERT INTO t_job (job_id) VALUES ($1)",
-                          param = list(seq_job$job_id))
-    jobs_inserted <- dbGetRowsAffected(rs)
-    dbClearResult(rs)
+    job_id <- open_job(conn)
 
-    stopifnot(jobs_inserted == 1)
-
-    cat(sprintf("Running new job: %d\n", seq_job$job_id))
+    cat(sprintf("Running new job: %d\n", job_id))
 
     for (sec_id in securities$id) {
         # load market data for sec
@@ -27,7 +46,7 @@ mdsnap <- function(host, port, dbname, user, password) {
         cat(sprintf("Loading symbol '%s'\n", sec))
         secds <- getSymbols(sec, auto.assign = FALSE)
         # print(secds)
-        dbds <- data.frame(security_id = sec_id, job_id = seq_job$job_id,
+        dbds <- data.frame(security_id = sec_id, job_id = job_id,
                            quote_date = index(secds),
                            quote_open = coredata(secds)[,1],
                            quote_high=coredata(secds)[,2],
@@ -39,4 +58,6 @@ mdsnap <- function(host, port, dbname, user, password) {
                     nrow(dbds), sec))
         dbWriteTable(conn, "t_quote", dbds, row.names=FALSE, append = TRUE)
     }
+
+    complete_job(conn, job_id)
 }
