201567fa642811c884d8f078642b8924a8f930d7
Alexander Dovzhikov <alexander.dovzhikov@gmail.com>
2019-06-04 11:41:48 +1000

    Tidying package dependencies

--- R/mdsnap.R
+++ R/mdsnap.R
@@ -1,6 +1,8 @@
-library(DBI)
-library(RPostgreSQL)
-library(quantmod)
+# library(DBI)
+# library(RPostgreSQL)
+# library(quantmod)
+
+#' @importFrom DBI dbSendStatement dbGetRowsAffected dbClearResult
 
 open_job <- function(conn) {
     # create a new job
@@ -14,6 +16,8 @@ open_job <- function(conn) {
     seq_job$job_id
 }
 
+#' @importFrom DBI dbSendStatement dbGetRowsAffected dbClearResult
+
 complete_job <- function(conn, job_id, job_status) {
     # complete a job
     rs <- dbSendStatement(conn, "UPDATE t_job
@@ -24,10 +28,27 @@ complete_job <- function(conn, job_id, job_status) {
     jobs_updated <- dbGetRowsAffected(rs)
     dbClearResult(rs)
 
-    sprintf(cat("Job %d is %s\n", job_id, job_status))
+    cat(sprintf("Job %d is %s\n", job_id, job_status))
 
     stopifnot(jobs_updated == 1)
 }
+
+#' Market Data Snap
+#'
+#' This function snaps current market data snapshot for the symbols universe
+#' and puts them into PostgreSQL database. The universe is configured in the
+#' database itself and needs to be set up manually.
+#' @param host Database host.
+#' @param port Database port
+#' @param dbname Database name.
+#' @param user Database user.
+#' @param password Database password.
+#' @export
+#' @importFrom DBI dbConnect dbDisconnect dbGetQuery dbWriteTable
+#' @importFrom RPostgreSQL PostgreSQL
+#' @importFrom quantmod getSymbols
+#' @importFrom zoo coredata index
+#' @importFrom xts make.index.unique
 
 mdsnap <- function(host, port, dbname, user, password) {
     conn <- dbConnect(RPostgreSQL::PostgreSQL(), user = user, password = password,
@@ -47,7 +68,8 @@ mdsnap <- function(host, port, dbname, user, password) {
             # load market data for sec
             sec <- securities[securities$id == sec_id, "name"]
             cat(sprintf("Loading symbol '%s'\n", sec))
-            secds <- getSymbols(sec, auto.assign = FALSE)
+            # load symbol and suppress wasrnings about missing values
+            secds <- suppressWarnings(getSymbols(sec, auto.assign = FALSE))
             # remove duplicate elements (sometimes happens)
             secds <- make.index.unique(secds, drop = TRUE)
 
@@ -72,6 +94,24 @@ mdsnap <- function(host, port, dbname, user, password) {
 
     complete_job(conn, job_id, job_status)
 }
+
+#' Market Data Load
+#'
+#' This function loads the specified set of sybmols from the database
+#' into an xts object.
+#' @param symbols A set of symbols to extract, must be specified explicitly.
+#' @param asof Allows to specify a snapshot to extract, NULL means to latest.
+#' @param features Features to extract.
+#' @param host Database host.
+#' @param port Database port
+#' @param dbname Database name.
+#' @param user Database user.
+#' @param password Database password.
+#' @export
+#' @importFrom DBI dbConnect dbDisconnect dbGetQuery
+#' @importFrom RPostgreSQL PostgreSQL
+#' @importFrom xts xts
+#' @author Alexander Dovzhikov, \email{alexander.dovzhikov@gmail.com}
 
 mdload <- function(symbols, asof = NULL,
                    features = c("open", "high", "low", "close", "volume", "adjusted"),
@@ -99,7 +139,7 @@ mdload <- function(symbols, asof = NULL,
         sql <- paste0("SELECT quote_date", lst_features, " FROM t_quote
                       WHERE security_id = $1 and job_id = $2")
         mdata <- dbGetQuery(conn, sql, list(sec$security_id, j$job_id))
-        colnames(mdata) <- c(data, features)
+        colnames(mdata) <- c("date", features)
         xts(mdata[, -1], order.by = mdata[, 1])
     })
     names(result) <- symbols
